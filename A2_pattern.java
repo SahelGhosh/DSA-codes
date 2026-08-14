aacb50272c3c25b3f460ef1f91415588c9987eab
sahel <sahelghosh7@gmail.com>
2026-08-14 12:37:26 +0530

    This is just a basic pattern

---
+++ A2_pattern.java
@@ -0,0 +1,9 @@
+public class A2_pattern {
+    public static void main(String[] args) {
+        System.out.println("* * * *");
+        System.out.println("* * * ");
+        System.out.println("* * ");
+        System.out.println("* ");
+    }
+}
+ 
