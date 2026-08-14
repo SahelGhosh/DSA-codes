3d9782125d82aa65e09ed92edea8fa6596f686f0
sahel <sahelghosh7@gmail.com>
2026-08-14 12:30:19 +0530

    1st java program written by me

---
+++ A1_helloworld.java
@@ -0,0 +1,7 @@
+public class A1_helloworld {
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
