4d4d95165bef9d6bd685650433d947a710ce7477
sahel <sahelghosh7@gmail.com>
2026-08-17 20:07:17 +0530

    Usr input variables

---
+++ A3_variable.java
@@ -0,0 +1,11 @@
+public class A3_variable {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Tony stark";
+        System.out.println(name);
+    }
+}
