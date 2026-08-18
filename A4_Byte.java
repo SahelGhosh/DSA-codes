80ef34a73427152e9da07802c2a8d39dddad30ae
sahel <sahelghosh7@gmail.com>
2026-08-18 22:17:24 +0530

    Variables

---
+++ A4_Byte.java
@@ -0,0 +1,17 @@
+public class A4_Byte {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float price = 10.5;
+        int number = 12;
+
+        //long for integer
+        //double for longer decimal number
+
+        short n = 240;
+    }
+}
